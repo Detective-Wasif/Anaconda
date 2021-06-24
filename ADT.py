7f8ae19a41fc0291e570d1b43d15189842f05ac7
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-06-24 23:11:44 +0600

    Update and rename document.py to ADT.py

--- ADT.py
+++ ADT.py
@@ -1,3 +1,6 @@
+# This is an internal module which contains abstract data structures used only by the interpreter itself
+# Document, Stack
+
 class Document:
   def __init__(self,x=0,y=0,wfill='-',hfill='|',corner='+',fill=' '):
     self.board=[]
@@ -14,14 +17,20 @@ class Document:
     return {attr:getattr(self,attr) for attr in self.props}
   def __str__(self):
     return "\n".join("".join(line) for line in self.board)
+  def update(self):
+    self.pos=(self.x,self.y)
   def downright(self,n):
-    for coord in range(n):
+    for coord in range(self.x,n):
       try:
         self.board[coord]
       except IndexError:
         self.board.append([self.fill]*(coord+1))
         self.board[coord][coord]='\\'
+        self.x+=1
+        self.y+=1
+    self.update()
 doc=Document()
 print(doc.pulse())
 doc.downright(5)
 print(str(doc))
+print(doc.pos)
