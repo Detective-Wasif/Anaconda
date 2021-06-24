33b41ddd067b633dd7b92b8452879501c0bfdd24
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-06-24 23:08:44 +0600

    Update document.py

--- document.py
+++ document.py
@@ -1,6 +1,6 @@
 class Document:
   def __init__(self,x=0,y=0,wfill='-',hfill='|',corner='+',fill=' '):
-    self.board=[[]]
+    self.board=[]
     self.x=x
     self.y=y
     self.pos=(self.x,self.y)
@@ -14,6 +14,14 @@ class Document:
     return {attr:getattr(self,attr) for attr in self.props}
   def __str__(self):
     return "\n".join("".join(line) for line in self.board)
+  def downright(self,n):
+    for coord in range(n):
+      try:
+        self.board[coord]
+      except IndexError:
+        self.board.append([self.fill]*(coord+1))
+        self.board[coord][coord]='\\'
 doc=Document()
 print(doc.pulse())
+doc.downright(5)
 print(str(doc))
