a2f7d7fff4108ec04a72d124f88104a93b2109a8
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-06-27 22:14:28 +0600

    Bugfree working lexer now

--- lexer.py
+++ lexer.py
@@ -1,5 +1,6 @@
-s='`Hello``World`+1.45-5`Anacon\`da`[`getch`[`a`|`b`01wasif]|`else`]wasif{0{1{2}}}99(loop`oi`)vivo'
+s='1[2|`yu`[4|5]]4(`repro me`)viƛ`full-blown map`12(see);λ(`variadic`{`arity`(`function`)})ƛ`it works now!!!`;;'
 
+# My poor man's parser for Anaconda written in pure Python (But it gets the job done)
 class Token:
   def __init__(self,Type):
     self.command=Type
@@ -44,7 +45,15 @@ def lexer(s):
        cap+=s[idx]
        idx+=1
      pair=cap[:-1]
+     free=0
      branch=pair.rfind('|')
+     for a,b in enumerate(pair):
+       if b=='[':
+         free+=1
+       if b==']':
+         free-=1
+       if (not free) and (b=='|'):
+         branch=a
      pair=[Token('if')]+[*map(lexer,[pair[:branch],pair[branch+1:]])]
      lex.append(pair)
      continue
@@ -61,7 +70,7 @@ def lexer(s):
        idx+=1
      block=cap[:-1] 
      catch=[Token('while')]+[lexer(block)]
-     lex.append(catch)  
+     lex.append(catch)    
      continue
     if s[idx]=='(':
      cap=''
@@ -76,17 +85,59 @@ def lexer(s):
        idx+=1
      block=cap[:-1] 
      catch=[Token('for')]+[lexer(block)]
-     lex.append(catch)     
+     lex.append(catch)    
      continue
     if s[idx]=='v':
-      lex.append([Token('vectorise')]+[Token(s[idx+1])])
-      idx+=2
+      idx+=1
+      catch=[Token('vectorise')]+lexer(s[idx])
+      lex.append(catch)
+      idx+=1
       continue
+    if s[idx]=='ƛ':
+     cap=''
+     idx+=1
+     cup=1
+     infunc=False
+     while cup:
+       if s[idx]=='λ':
+         infunc=True
+       if s[idx]=='ƛ':
+         cup+=1
+       if (s[idx]==';') and (not infunc):
+         cup-=1
+       if (s[idx]==';') and infunc:
+         infunction=False     
+       cap+=s[idx]
+       idx+=1
+     block=cap[:-1] 
+     catch=[Token('map')]+[lexer(block)]
+     lex.append(catch)    
+     continue
+    if s[idx]=='λ':
+     cap=''
+     idx+=1
+     cup=1
+     inmap=False
+     while cup:
+       if s[idx]=='ƛ':
+         inmap=True
+       if s[idx]=='λ':
+         cup+=1
+       if (s[idx]==';') and (not inmap):
+         cup-=1
+       if (s[idx]==';') and inmap:
+         inmap=False
+       cap+=s[idx]
+       idx+=1
+     block=cap[:-1] 
+     catch=[Token('function')]+[lexer(block)]
+     lex.append(catch)    
+     continue
     lex.append(Token(s[idx]))
     idx+=1
-
- 
   return lex
 
 
 print(lexer(s))
+for command in lexer(s):
+  print(command)
