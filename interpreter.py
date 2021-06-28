a1bc754fdddf0fb5528f31910c6d096d954085b7
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-06-28 22:18:24 +0600

    Update and rename builtins.py to interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,81 @@
+import os
+import re
+import math
+import random
+import string
+import time
+import urllib.request
+import base64
+import functools
+import itertools
+import collections
+import hashlib
+import operator
+import turtle
+import numpy as np
+import sympy
+from lexer import *
+from datetime import date
+from datetime import datetime as dt
+class Stack:
+  def __init__(self,prep=[]):
+    self.stack=prep
+  def push(self,val):
+    self.stack.append(val)
+  def pop(self):
+    return self.stack.pop()
+  def peek(self):
+    return self.stack[-1]
+  def top(self):
+    return self.stack.pop(0)
+  def over(self):
+    return self.stack[0]
+  def len(self):
+    return len(self.stack)
+  def prepend(self,val):
+    self.stack.insert(0,val)
+  def dup(self):
+    self.push(stack[-1])
+  def trip(self):
+    self.stack.extend([stack[-1]]*2)
+  def swap(self):
+    self.stack[-2],self.stack[-1]=self.stack[-1],self.stack[-2]
+  def rotl(self,units=1):
+    for i in range(units+1):
+      self.push(self.top())
+  def rotr(self,units=1):
+    for i in range(units+1):
+      self.prepend(self.pop())
+  def fetch(self,pos):
+    self.push(self.stack[pos])
+  def sort(self):
+    self.stack.sort()
+  def reverse(self):
+    self.stack=self.stack[::-1]
+  def join(self,delim=''):
+    self.push(delim.join(map(str,self.stack)))
+  def __str__(self):
+   self.join(' ')
+   return self.pop()
+stack=Stack()
+variables=[]
+register=string.ascii_lowercase
+functions=[]
+def product(array):
+  return functools.reduce(operator.__mul__,array)
+def transpose(array,filler=0):
+  return list(map(list,itertools.zip_longest(array,fillvalue=filler)))
+def integer_partitions(number,I=1):
+  partitions=[[number]]
+  for i in range(I,number//2+1):
+    for p in integer_partitions(number-i,i):
+      partitions.append([i]+p)
+  return partitions
+def partitions(iterable):
+  ret=[]
+  for i in range(1,len(iterable)):
+    for sub in partitions(iterable[i:]):
+      sub.insert(0,iterable[:i])
+      ret.append(sub)
+  ret.append([iterable])
+  return ret
