2cbbc247e38100ac2ba91fbfa50d3a63e5529cba
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-07-06 11:25:58 +0600

    I/O fixed

--- server.py
+++ server.py
@@ -2,17 +2,23 @@ from flask import Flask,request,render_template
 from interpreter import *
   
 app = Flask(__name__)   
-@app.route('/', methods =["GET","POST"])
+@app.route('/',methods=["GET","POST"])
 def gfg():
   if request.method=="POST":
     script=request.form.get('code')
     Input=request.form.get('input')
-    Input=[eval(i) for i in Input.split("\n")]
+    if Input:
+      Input=[eval(i) for i in Input.split("\n")]
+    else:
+      Input=[]
     data=execute(script,[''],Input)
     fine="".join(data)
     l=str(len(script))
     leng=l+' bytes, '+l+' chars, (SBCS)'
-    return render_template('index.html',output=fine,code=script,leng=leng,input="\n".join(map(str,Input)))        
+    send="\n".join(map(str,Input))
+    if send=='pass':
+      send=''
+    return render_template('index.html',output=fine,code=script,leng=leng,input=send)        
   return render_template("index.html")
 if __name__=='__main__':
    app.run(port=7777,debug=True)
