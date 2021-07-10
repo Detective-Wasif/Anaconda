0febca60a8535eb0eab57bad1abcab7c07129de3
Detective-Wasif <81944371+Detective-Wasif@users.noreply.github.com>
2021-07-10 10:37:15 +0600

    Pass flags to execute()

--- server.py
+++ server.py
@@ -7,18 +7,25 @@ def gfg():
   if request.method=="POST":
     script=request.form.get('code')
     Input=request.form.get('input')
+    flags=request.form.get('flags')
     if Input:
       Input=[eval(i) for i in Input.split("\n")]
     else:
       Input=[]
-    data=execute(script,[''],Input)
-    fine="".join(data)
+    data=execute(script,[''],Input,[*flags])
+    fine="".join(data[0])
     l=str(len(script))
     leng=l+' bytes, '+l+' chars, (SBCS)'
     send="\n".join(map(str,Input))
     if send=='pass':
       send=''
-    return render_template('index.html',output=fine,code=script,leng=leng,input=send)        
+    debug=request.form.get('debug')
+    transpiled="".join(data[1])
+    if debug:
+      fine+="\n"
+      fine+="=====================================\n"
+      fine+=transpiled
+    return render_template('index.html',output=fine,code=script,leng=leng,input=send,trans=transpiled)        
   return render_template("index.html")
 if __name__=='__main__':
    app.run(port=7777,debug=True)
